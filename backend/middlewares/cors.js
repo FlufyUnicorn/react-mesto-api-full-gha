67fee9e36dfcbb81e45d2b64915a16a0dee469ce
Анастасия Зимина <Asya.Zimina777@mail.ru>
2023-07-27 23:57:34 +0300

    fix allowedCors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,7 +1,8 @@
 const allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
-  'localhost:3000',
+  'https://mesto-project.students.nomoreparties.sbs',
+  'http://mesto-project.students.nomoreparties.sbs',
+  'https://localhost:3000',
+  'http://localhost:3000',
 ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
